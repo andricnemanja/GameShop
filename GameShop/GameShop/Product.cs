60c533b0496ee91b19bde7d2a49053532d7db5ba
Nemanja Andric <nandric00@gmail.com>
2022-05-03 16:06:30 +0200

    Change product table to show discount and tax amount

--- GameShop/GameShop/Product.cs
+++ GameShop/GameShop/Product.cs
@@ -12,10 +12,14 @@ namespace GameShop
         public int UPC { get; set; }
         public double Price { get; set; }
         public double FinalPrice { get; set; }
+        public double DiscountAmount { get; set; }
+        public double TaxAmount { get; set; }
 
         public void Update(double newTax, double newDiscount)
         {
             FinalPrice = Math.Round(Price + Price * newTax / 100 - Price * newDiscount / 100, 2);
+            DiscountAmount = Math.Round(Price * newDiscount / 100, 2);
+            TaxAmount = Math.Round(Price * newTax / 100, 2);
         }
     }
 }
